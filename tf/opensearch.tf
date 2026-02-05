81aef00f40cecb2268762ef2ee0d554f8c7816e5
SebastianRoseneck <55637012+SebastianRoseneck@users.noreply.github.com>
2026-02-05 17:44:07 +0100

    Add full Terraform setup for automated Camunda 8 deployment with dependencies

--- tf/opensearch.tf
+++ tf/opensearch.tf
@@ -1,11 +1,14 @@
-resource "stackit_opensearch_instance" "camunda8" {
+resource "stackit_opensearch_instance" "camunda" {
   project_id = var.project_id
   name       = "camunda-opensearch"
   version    = "2"
-  plan_name  = "stackit-opensearch-1.4.10-single"
+  plan_name  = var.opensearch_plan
+  parameters = {
+    sgw_acl = join(",", stackit_ske_cluster.camunda.egress_address_ranges)
+  }
 }
 
-resource "stackit_opensearch_credential" "camunda8_credential" {
+resource "stackit_opensearch_credential" "camunda" {
   project_id  = var.project_id
-  instance_id = stackit_opensearch_instance.camunda8.instance_id
+  instance_id = stackit_opensearch_instance.camunda.instance_id
 }
